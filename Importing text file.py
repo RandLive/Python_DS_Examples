dade25c446ecfd92188b08b7299ca0da8f0a1e6f
Mel <dreamflg_dv@hotmail.com>
2017-05-25 19:25:55 +0200

    v3

---
+++ Importing text file.py
@@ -0,0 +1,16 @@
+# Importing text file
+
+# Open a file: file
+file = open('moby_dick.txt', 'r')
+
+# Print it
+print(file.read())
+
+# Check whether file is closed
+print(file.closed)
+
+# Close file
+file.close()
+
+# Check whether file is closed
+print(file.closed)
